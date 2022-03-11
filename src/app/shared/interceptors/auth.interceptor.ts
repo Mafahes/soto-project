3e1f36667ed4b7856f7f4978c64dd1bd6ed72d5b
Mafahes <vadm11.12@mail.ru>
2022-03-11 10:21:39 +0300

    upd

--- src/app/shared/interceptors/auth.interceptor.ts
+++ src/app/shared/interceptors/auth.interceptor.ts
@@ -3,10 +3,15 @@ import {Observable, throwError} from 'rxjs';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {catchError} from 'rxjs/operators';
 import {Injectable} from '@angular/core';
+import { AppComponent } from '../../app.component';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private snackBar: MatSnackBar) {}
+  constructor(
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {}
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('api_token');
     const cloned = req.clone({
@@ -14,6 +19,10 @@ export class AuthInterceptor implements HttpInterceptor {
     });
     return next.handle(cloned).pipe(
       catchError((err: HttpErrorResponse) => {
+        if (err.status === 401) {
+          localStorage.removeItem('api_token');
+          document.location.reload();
+        }
         this.snackBar.open('Ошибка ' + err.status, 'OK', {
           duration: 1000,
           panelClass: ['snack-bar-card']
